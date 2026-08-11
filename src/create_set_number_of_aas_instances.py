7ccf4959a8eaffbc2a180bdf5a521d1bde89fc7a
Produktentstehung <github@mail.uni-paderborn.de>
2026-08-11 17:29:25 +0200

    Update create_set_number_of_aas_instances.py

--- src/create_set_number_of_aas_instances.py
+++ src/create_set_number_of_aas_instances.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2025 Heinz Nixdorf Institute
+# Copyright (c) 2025 Paderborn University
+# Copyright (c) 2025 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
 """
-create_aas_v17.py - Fixes for [Content_Types].xml, HandoverDoc enhancements, and Models3D styling.
+create_set_number_of_aas_instances.py - Creates a defined number of instance
+AAS from a preconfigured type AAS. Handles [Content_Types].xml, HandoverDoc
+enhancements, and Models3D styling.
 """
 import json, sys, os, shutil, zipfile
 from pathlib import Path
